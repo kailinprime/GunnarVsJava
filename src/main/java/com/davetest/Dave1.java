0a80d66ecb2c34282f3eaa7c451cf90d9c5f6127
kailinprime <david.oplinger@gmail.com>
2011-11-15 20:41:35 -0600

    starting project

---
+++ src/main/java/com/davetest/Dave1.java
@@ -0,0 +1,12 @@
+package main.java.com.davetest;
+
+public class Dave1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Testing 123");
+	}
+
+}
---
+++ src/test/java/com/davetest/Dave1Test.java
@@ -0,0 +1,17 @@
+package test.java.com.davetest;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Dave1Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
